7eabc8fa9c3b7e24aad9927c48f4f6d6677a2880
99kenny <helloiamkenny@gmail.com>
2022-07-20 14:29:13 +0900

    project settings

---
+++ src/main/java/com/woodongleee/WooDongLeeeApplication.java
@@ -0,0 +1,13 @@
+package com.woodongleee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooDongLeeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooDongLeeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woodongleee/WooDongLeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodongleee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooDongLeeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
