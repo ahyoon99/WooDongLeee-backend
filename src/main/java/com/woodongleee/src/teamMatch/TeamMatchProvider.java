1fca7b8cc3eeedc76b6c16db01efaf1ba5f18957
ahyoon99 <kimay9623@naver.com>
2022-08-13 01:25:12 +0900

    feat/add_game_result

--- src/main/java/com/woodongleee/src/teamMatch/TeamMatchProvider.java
+++ src/main/java/com/woodongleee/src/teamMatch/TeamMatchProvider.java
@@ -30,4 +30,35 @@ public class TeamMatchProvider {
         int userIdx = teamMatchDao.whoWritePost(matchPostIdx); // 매칭글 idx로 매칭글 작성한 유저 idx 찾기
         return userIdx;
     }
+
+    // teamScheduleIdx에서 homeIdx(팀 idx) 가져오기
+    public int selectHomeIdxByTeamScheduleIdx(int teamScheduleIdx) throws BaseException{
+        // 존재하지 않는 팀 일정(경기)입니다.
+        if(teamMatchDao.existTeamMatch(teamScheduleIdx) == 0){    // 탐 일정이 존재하지 않는 경우
+            throw new BaseException(BaseResponseStatus.SCHEDULE_DOES_NOT_EXIST);
+        }
+        int homeIdx = teamMatchDao.selectHomeIdxByTeamScheduleIdx(teamScheduleIdx);
+        return homeIdx;
+    }
+
+    // teamScheduleIdx에서 awayIdx(팀 idx) 가져오기
+    public int selectAwayIdxByTeamScheduleIdx(int teamScheduleIdx) throws BaseException{
+        // 존재하지 않는 팀 일정(경기)입니다.
+        if(teamMatchDao.existTeamMatch(teamScheduleIdx) == 0){    // 탐 일정이 존재하지 않는 경우
+            throw new BaseException(BaseResponseStatus.SCHEDULE_DOES_NOT_EXIST);
+        }
+        int awayIdx = teamMatchDao.selectAwayIdxByTeamScheduleIdx(teamScheduleIdx);
+        return awayIdx;
+    }
+
+    // 팀의 리더 userIdx 가져오기
+    public int selectLeaderIdxByTeamIdx(int homeIdx) throws BaseException{
+        // 존재하지 않는 팀입니다.
+        if(teamMatchDao.existTeamInfo(homeIdx)==0){     // 팀이 존재하지 않는 경우
+            throw new BaseException(BaseResponseStatus.TEAM_DOES_NOT_EXIST);
+        }
+        int userIdx = teamMatchDao.selectLeaderIdxByTeamIdx(homeIdx);
+        return userIdx;
+    }
+
 }
